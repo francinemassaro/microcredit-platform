68aca91f028a6cff1a3ce842b3aa83cc8bbb9cb3
francinemassaro <francine.massaro@gmail.com>
2025-05-10 11:01:56 -0300

    Projeto inicial com estrutura multi-módulo e POM pai.

---
+++ src/main/java/com/microcredit/platform/MicrocreditPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.microcredit.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocreditPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocreditPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microcredit/platform/MicrocreditPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcredit.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocreditPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

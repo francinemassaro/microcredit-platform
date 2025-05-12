19393218594427583e93a05c6ccfa7a721b394a7
francinemassaro <francine.massaro@gmail.com>
2025-05-11 21:23:13 -0300

    feat(user-service): login DTOs request and response

---
+++ user-service/src/main/java/com/microcredit/user/dto/response/LoginResDTO.java
@@ -0,0 +1,13 @@
+package com.microcredit.user.dto.response;
+
+public class LoginResDTO {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}

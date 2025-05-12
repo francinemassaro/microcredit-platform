c944aafad426d83ef0c7a1f3002bd1cf67e81c24
francinemassaro <francine.massaro@gmail.com>
2025-05-11 21:22:25 -0300

    feat(user-service): [auth] configure SecurityConfig for JWT

--- user-service/src/main/java/com/microcredit/user/config/SecurityConfig.java
+++ user-service/src/main/java/com/microcredit/user/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.microcredit.user.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +15,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .build();
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        // todo o resto exige autenticação
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration cfg
+    ) throws Exception {
+        return cfg.getAuthenticationManager();
     }
 }
